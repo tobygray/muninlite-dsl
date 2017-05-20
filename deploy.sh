aeaaf23a6c219d2fff0b7f0563edbc8471c12bb5
Toby Gray <toby.gray@gmail.com>
2017-05-20 15:59:18 +0100

    Adding caching of DSL status

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-rm dsl_*~
-scp dsl_* root@${1:-192.168.100.3}:/usr/sbin/munin-node-plugin.d
+rm dsl_*~ >/dev/null 2>&1 || true
+scp .get_status dsl_* root@${1:-192.168.100.3}:/usr/sbin/munin-node-plugin.d
